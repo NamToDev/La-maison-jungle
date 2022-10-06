766ea9f30263931134999f01f1155f0b860df1ab
NamToDev <oessabil@gmail.com>
2022-10-06 13:36:04 +0200

    My first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/App.css";
+import Banner from "./Banner";
+import Cart from "./Cart";
+import ShoppingList from "./ShoppingList";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Banner />
+      <Cart />
+      <ShoppingList />
+    </React.Fragment>
+  );
+}
+
+export default App;
